679d28f90267f0e00eac17a0f12159e4c885dc75
Taketomo Oishi <takeweb@mac.com>
2023-05-14 18:53:09 +0900

    先週額主文

---
+++ svelte-book-first-tutorial/src/App.svelte
@@ -0,0 +1,9 @@
+<header class="header">
+  <a class="header-title" href="/">Svelte EC</a>
+  <nav>
+    <ul class="header-links">
+      <li>ようこそゲストさん</li>
+      <li><a href="/cart">カート (0)</a></li>
+    </ul>
+  </nav>
+</header>
